961c4a045ac230330513ee696832c11bf4673ff9
Priyam-28 <barikpriyam28@gmail.com>
2023-11-05 18:20:18 +0530

    First Commit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/barik/opencv
+# Build directory: C:/Users/barik/opencv/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
